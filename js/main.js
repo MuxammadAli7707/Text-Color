c8a8183ed5e4645cf29c540693749571288bf6b3
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-05-05 07:54:42 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const area = document.getElementById("area");
 
 area.addEventListener("keyup", (e) => {
   if(area.value.includes("#")){
-    area.value.style.color = "blue";
+    area.style.color = "blue";
     console.log(area.value);
   }
 });
